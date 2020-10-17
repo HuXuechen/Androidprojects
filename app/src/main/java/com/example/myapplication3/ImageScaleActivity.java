c2205797a09c4e0934952811954bc2b47f8359bb
HuXuechen <hxc13231031997@icloud.com>
2020-10-17 09:41:46 +0800

    第三次作业

---
+++ app/src/main/java/com/example/myapplication3/ImageScaleActivity.java
@@ -0,0 +1,13 @@
+package com.example.myapplication3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImageScaleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_scale);
+    }
+}
